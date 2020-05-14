7ec11bfff10a1eeabebc03e229dd7838401d4e3d
Wang Yin <kris19960712@gmail.com>
2020-05-14 10:43:52 +0800

    fix api to do get

--- src/scripts/react/apiClient.jsx
+++ src/scripts/react/apiClient.jsx
@@ -4,7 +4,6 @@ export default {
     // get ac-guide sheet data
     GetList: (args) => {
         const url = 'https://script.google.com/macros/s/AKfycbz59fQboc4DbvelUIua8H8ANCvqRTqjG042hhdHdzHyzwY8kqo/exec'
-        return axios.post(url, args)
-        //return axios.get(url, { params: args })
+        return axios.get(url, { params: args })
     }
 }
